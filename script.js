4d3519e7f44ed2b2f7f0e75034052804ecf6fdfb
Xenia <tul.xenia97@yandex.ru>
2020-02-07 15:44:23 +0300

    Commit

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 //Создаем переменные
-
+// let calendar=document.querySelector(".calendar")
+let container = document.querySelector(".container");
 let yearMon = document.querySelector(".yearMon");
 let prevButton = document.querySelector("#prev");
 let nextButton = document.querySelector("#next");
 let dates = document.querySelector(".Dates");
+let checkList = document.querySelector(".checkList");
+let task = document.createElement("input");
+checkList.append(task);
+task.type = "text";
+task.placeholder = "Введите дело";
+let button = document.createElement("button");
+checkList.append(button);
+button.innerHTML = "Добавить заметку";
 let otherMonth = 0; //счетчик месяцев
 let months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
 create(); //вызываем функцию, которая создает календарь
@@ -28,11 +37,7 @@ function create() {
     let month = date.getMonth();
     let lastDayMonth = new Date(year, month + 1, 0).getDate()
     let startDay = new Date(year, month, 1).getDay();
-    // let dayWeek=new Date();
-    // // if (dayWeek === 0) {
-    // //     dayWeek = 7
-    // // }
-    // if((dayWeek.getDay() == 0) || (dayWeek.getDay()== 6)){
+
     if (startDay === 0) {
         startDay = 7
     }
@@ -41,28 +46,51 @@ function create() {
     if (otherMonth == 0) {
         today = date.getDate();
     }
-    let html = "";
+
+
+
+    let days = " ";
     for (i = 1; i < startDay; i++) {
-        html += '<div class="day next"></div>';
+        days += '<div class="day next"></div>';
     }
     for (i = 1; i <= lastDayMonth; i++) {
         let st = 'day';
         if (i === today) {
             st += ' today '
+
         }
+
         let dayWeek = new Date(date.getFullYear(), date.getMonth(), i)
         if ((dayWeek.getDay() == 0) || (dayWeek.getDay() == 6)) {
             st += ' week '
+
+
         }
 
-        html += '<div class="' + st + '">' + i + '</div>';
-
+        days += '<div class="' + st + '">' + i + '</div>';
+        dates.innerHTML = days;
     }
 
-    dates.innerHTML = html;
+
 
     yearMon.innerHTML = date.getFullYear() + ' ' +
         months[date.getMonth()];
-}
 
-let da = document.querySelector(".day");
+
+}
+// dates.addEventListener("click", showChecklist)
+
+
+// function showChecklist() {
+//     checkList.style.display = "block";
+// }
+
+// function createCheckList() {
+//     let ulList = document.createElement('ul');
+//     checkList.append(ulList);
+
+//     let liList = document.createElement("li");
+//     ulList.appendChild(liList);
+//     liList.innerHTML = task.value;
+// }
+// button.addEventListener("click", createCheckList)
