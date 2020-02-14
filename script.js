727c9cd9d2023b8b7eba75d0f565e78e988623ae
Xenia <tul.xenia97@yandex.ru>
2020-02-14 18:58:29 +0300

    commit

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
+let container = document.querySelector(".container");
+
+let row = document.createElement("div");
+container.append(row);
+row.classList.add('row');
+for (i = 1; i <= 6; i++) {
+    let week = document.createElement("div");
+    row.append(week);
+    week.classList.add('week');
+    for (j = 1; j < 8; j++) {
+        let days = document.createElement("div");
+        week.append(days);
+        days.classList.add('days');
+
+        days.addEventListener("click", createForm);
+    }
+}
 let dates = document.querySelector(".Dates");
 let days = document.querySelectorAll(".days");
 let week = document.querySelector(".week")
+let monthYear = document.getElementById('monthYear');
+let nextMonth = document.getElementById('arrowNextMonth');
+let prevMonth = document.getElementById('arrowPrevMonth');
 let date = new Date();
 let monthFullDays = new Date(date.getFullYear(), date.getMonth() + 1, 0); //последний день месяца
 let currentYear = date.getFullYear(); //текущий год
 let currentMonth = date.getMonth(); //текущий месяц
 let startDay = new Date(currentYear, currentMonth, 1).getDay(); //день недели первого дня месяца
-if (startDay === 0) {
-    startDay = 7
-}
 let months = ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь'];
 monthList(startDay, 1, monthFullDays.getDate(), days)
 
 function monthList(beg, from, to, parent) {
-    var j;
+    let j;
     if (beg === 0) {
         beg = 7
     }
@@ -26,42 +43,28 @@ function monthList(beg, from, to, parent) {
         parent[j].innerHTML = i;
 
 
-        if (i == date.getDate() && date.getMonth() == currentMonth) {
-            parent[j].classList.add('today');
-        }
+        // if (i == date.getDate() && date.getMonth() == currentMonth) {
+        //     parent[j].classList.add('today');
+        // }
 
-        if (weekends(i)) parent[j].classList.add('weekend');
+        // if (weekends(i)) parent[j].classList.add('weekend');
     }
 
 }
 
-// function getDay(date) {
-//     let day = date.getDay();
-//     if (day == 0) {
-//         day = 7
-//     }
-//     return day - 1
-// }
 
-function weekends(i) {
-    let currentDay = new Date(date.getFullYear(), date.getMonth(), i);
-    let todayDay = currentDay.getDay();
-    return todayDay == 0 || todayDay == 6;
-}
+// function weekends(i) {
+//     let currentDay = new Date(date.getFullYear(), date.getMonth(), i);
+//     let todayDay = currentDay.getDay();
+//     return todayDay == 0 || todayDay == 6;
+// }
 
 changeMonth(days)
 
 function changeMonth(parent) {
-    var monthYear = document.getElementById('monthYear');
-    var nextMonth = document.getElementById('arrowNextMonth');
-    var prevMonth = document.getElementById('arrowPrevMonth');
-    // let dayListParent = document.querySelector(parent);
-
     monthYear.innerHTML = months[currentMonth] + ' ' + currentYear;
-
     nextMonth.addEventListener('click', function() {
-
-        for (var i = 0; i < days.length; i++) {
+        for (i = 0; i < days.length; i++) {
             parent[i].innerHTML = " "; // Вызов myNodeList.item(i) необязателен в JavaScript
         } // to
         if (currentMonth == months.length - 1) {
@@ -76,10 +79,7 @@ function changeMonth(parent) {
         monthList(newMonBeg, 1, currentMonthDaysNum.getDate(), days)
 
     });
-
-
     prevMonth.addEventListener('click', function() {
-
         for (var i = 0; i < days.length; i++) {
             days[i].innerHTML = " "; // Вызов myNodeList.item(i) необязателен в JavaScript
         } // to clear parent and insert new month days
@@ -95,4 +95,46 @@ function changeMonth(parent) {
 
         monthList(prevMonBeg, 1, currentMonthDaysNum.getDate(), days)
     })
+
+}
+let tasks = [];
+let daytask;
+
+function createForm() {
+    // let tasks=[];
+    this.classList.add("check");
+    daytask = this.textContent + " " + monthYear.textContent;
+    let taskForm = document.createElement("input");
+
+    document.body.append(taskForm);
+    taskForm.classList.add("taskForm");
+    taskForm.type = "text";
+    taskForm.placeholder = "Введите дело";
+    let button = document.createElement("button");
+    document.body.append(button);
+    button.classList.add("add");
+    button.innerHTML = "Добавить заметку";
+    let taskList = document.createElement("div");
+    taskList.classList.add("taskList");
+    document.body.append(taskList);
+    save()
+    console.log(daytask)
+}
+
+
+function save() {
+    let taskList = document.querySelector(".taskList");
+    let taskForm = document.querySelector(".taskForm");
+    let button = document.querySelector(".add")
+    button.addEventListener("click", function() {
+        let t = taskForm.value;
+        tasks.push(t)
+        let tList = " ";
+        for (i = 0; i < tasks.length; i++) {
+            tList += tasks[i];
+        }
+        taskList.innerHTML = tList;
+    })
+
+
 }
